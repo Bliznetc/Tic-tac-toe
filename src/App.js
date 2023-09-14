f7c001029cf0ed04f7d5ba9a2f8b720c7151b584
Igor Moroz <bliznetc2006@gmail.com>
2023-09-14 23:40:38 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick, color }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button className="square" onClick={onSquareClick} style={{ color: color }}>
       {value}
     </button>
   );
 }
 
-const n = 20;
-const m = 40;
+function ToggleButton({ onPlay }) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  function handleToggle() {
+    setIsChecked(!isChecked);
+    console.log("handle1: " + isChecked);
+    onPlay(isChecked); // Call onPlay with the new value
+  }
+
+  return (
+    <label className="switch">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => handleToggle()}
+      />
+      <span className="slider"></span>
+    </label>
+  );
+}
+
+const n = 3;
+const m = 3;
 
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
@@ -18,19 +39,15 @@ function Board({ xIsNext, squares, onPlay }) {
       return;
     }
     let erase = false;
-    if (squares[i]) {
-        erase = true;
-    }
-    
+
     const nextSquares = squares.slice();
     if (erase) {
       nextSquares[i] = null;
-    }
-    else {
+    } else {
       if (xIsNext) {
-        nextSquares[i] = 'X';
+        nextSquares[i] = "X";
       } else {
-        nextSquares[i] = 'O';
+        nextSquares[i] = "O";
       }
     }
     onPlay(nextSquares);
@@ -39,38 +56,56 @@ function Board({ xIsNext, squares, onPlay }) {
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
-    status = 'Winner: ' + winner;
+    status = "Winner: " + winner;
   } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  let forWinner = [null];
+  if (winner) {
+    forWinner = highlight(squares);
+  }
+
+  console.log(forWinner);
+  const isDraw = checkIsDraw(squares, winner);
+  if (isDraw) {
+    status = "It's a draw, quys";
+  }
 
   let items = Array.from({ length: n }, () => []);
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < m; j++) {
-      items[i].push(
-        <Square 
-          key={i*m + j}
-          value={squares[i * m + j]} 
-          onSquareClick={() => handleClick(i * m + j)} 
+      let color = "black";
+      if (forWinner.includes(i * m + j)) {
+        console.log(i * m + j);
+        if (winner === "X") {
+          color = "red";
+        } else {
+          color = "green";
+        }
+      }
+
+      const curSquare = (
+        <Square
+          key={i * m + j}
+          value={squares[i * m + j]}
+          onSquareClick={() => handleClick(i * m + j)}
+          color={color}
         />
-      )
+      );
+      items[i].push(curSquare);
     }
   }
 
   let currentBoard = [];
   for (let i = 0; i < n; i++) {
-    currentBoard.push(
-      <div className="board-row">
-        {items[i]}
-      </div>
-    )
+    currentBoard.push(<div className="board-row">{items[i]}</div>);
   }
 
   return (
     <>
       <div className="status">{status}</div>
-      <div className='Board'>{currentBoard}</div>
+      <div className="Board">{currentBoard}</div>
     </>
   );
 }
@@ -78,6 +113,7 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [history, setHistory] = useState([Array(n * m).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [isChecked, setChecked] = useState(false);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
@@ -91,26 +127,59 @@ export default function Game() {
     setCurrentMove(nextMove);
   }
 
-  
-  const moves = history.slice(0, history.length - 1).map((squares, index) => {
-    let description;
-    if (index > 0) {
-      description = 'Go to move #' + index;
-    } else {
-      description = 'Go to game start';
-    }
-    return (
-      <li key={index}>
-        <button onClick={() => jumpTo(index)}>{description}</button>
-      </li>
-    );
-  });
+  git remote add origin https://github.com/Bliznetc/Tic-tac-toe.git
+  function onChangeToggle() {
+    setChecked((prevIsChecked) => {
+      // Use the previous state value to calculate the new state
+      console.log("handle: " + prevIsChecked);
+      const newIsChecked = !prevIsChecked;
+      console.log("handle: " + newIsChecked);
+      return newIsChecked; // Return the new value to update the state
+    });
+  }
+
+  let moves;
+  if (isChecked) {
+    moves = history.slice(0, history.length).map((squares, index) => {
+      let description;
+      if (index > 0) {
+        if (index === history.length - 1) {
+          description = "Go to game start";
+        } else {
+          description = "Go to move #" + (history.length - index - 1);
+        }
+      } else {
+        return null;
+      }
+      return (
+        <li key={index}>
+          <button onClick={() => jumpTo(history.length - index - 1)}>
+            {description}
+          </button>
+        </li>
+      );
+    });
+  } else {
+    moves = history.slice(0, history.length - 1).map((squares, index) => {
+      let description;
+      if (index > 0) {
+        description = "Go to move #" + index;
+      } else {
+        description = "Go to game start";
+      }
+      return (
+        <li key={index}>
+          <button onClick={() => jumpTo(index)}>{description}</button>
+        </li>
+      );
+    });
+  }
   moves.push(
-    <p key="Your move">
+    <p className="Your move" key="Your move">
       You are at move {currentMove + 1}
     </p>
   );
-  
+
   return (
     <div className="game">
       <div className="game-board">
@@ -119,10 +188,13 @@ export default function Game() {
       <div className="game-info">
         <ol>{moves}</ol>
       </div>
+      <div>
+        <h1 className="toggleName">Sort moves in ascending order</h1>
+        <ToggleButton onPlay={onChangeToggle} />
+      </div>
     </div>
-  )
+  );
 }
-
 
 function calculateWinner(squares) {
   const lines = [
@@ -133,7 +205,7 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
@@ -144,13 +216,46 @@ function calculateWinner(squares) {
   return null;
 }
 
+function checkIsDraw(squares, winner) {
+  if (winner != null) {
+    return false;
+  }
 
-function MyComponent({ dataArray }) {
-  const joinedArray = dataArray.join(',');
-  return (
-    <div>
-      <h1>Array Elements:</h1>
-      <p className='output'>{joinedArray}</p>
-    </div>
-  );
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i] == null) {
+      return false;
+    }
+  }
+  return true;
 }
+
+function highlight(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  let cur = [];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      cur = [a, b, c];
+    }
+  }
+  return cur;
+}
+
+// function MyComponent({ dataArray }) {
+//   const joinedArray = dataArray.join(",");
+//   return (
+//     <div>
+//       <h1>Array Elements:</h1>
+//       <p className="output">{joinedArray}</p>
+//     </div>
+//   );
+// }
